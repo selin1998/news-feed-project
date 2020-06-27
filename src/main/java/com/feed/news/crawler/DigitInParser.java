4ee2bcfc3d43c66c606ac60ecf72e5757f30e2a7
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-06-28 02:13:22 +0400

    -- added parsers,need to be fixed

--- src/main/java/com/feed/news/crawler/DigitInParser.java
+++ src/main/java/com/feed/news/crawler/DigitInParser.java
@@ -26,42 +26,10 @@ public class DigitInParser extends News implements JsoupParser {
 
             articles.add(new Article(header, content, link, imageLink, date, Website.DigitIn));
         }
-//            System.out.println(header);
-//            System.out.println(content);
-//            System.out.println(link);
-//            System.out.println(imageLink);
-//            System.out.println(date);
 
         return articles;
     }
 }
-
-
-//1
-//for (Element element : elements) {
-//        String header = element.select(".product-desc").text();  //<a><div class="product-desc">
-//        String content = header;
-//        String link = element.getElementsByTag("a").first().attr("href");
-//        String imageLink = element.getElementsByTag("img").first().attr("src");
-//        String date = element.getElementsByTag("span").first().text();
-//
-//        articles.add(new Article(header, content, link, imageLink, date, Website.DigitIn));
-//        }
-
-
-//2
-//Document document = Jsoup.connect("https://www.digit.in/news/").get();
-//    Elements elements = document.getElementsByClass("Thumb-new");
-//        for (Element element : elements) {
-//                String header = element.select(".product-desc").text();  //<a><div class="product-desc">
-//                String content = element.select(".product-desc").text();
-//                String link = element.select("a").first().attr("href");
-//                String imageLink = element.select(".Search-product").first()
-//                .select("img").first().attr("src");
-//                String date = element.select("span").first().text();
-//
-//                articles.add(new Article(header, content, link, imageLink, date, Website.DigitIn));
-//                }
 
 
 
---
+++ src/main/java/com/feed/news/crawler/TechStartupsParser.java
@@ -0,0 +1,36 @@
+package com.feed.news.crawler;
+
+import lombok.SneakyThrows;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TechStartupsParser extends News implements JsoupParser {
+
+    static List<Article> articles = new ArrayList();
+
+    @SneakyThrows
+    @Override
+    public List<Article> getArticles() {
+        Document document = Jsoup.connect("https://techstartups.com/").get();
+        Elements elements = document.getElementsByClass("post");
+        for (Element element : elements) {
+            String header = element.select(".post_header_title").first().getElementsByTag("a").first().text();
+            String content = element.select(".post_header_title").first().getElementsByTag("p").first().text();
+            String link =element.select(".post_header_title").first()
+                    .getElementsByTag("a").first().attr("href");
+            //  String link =element.getElementsByTag("a").first().attr("abs:href");
+            String imageLink = element.getElementsByTag("img").first().attr("src");  //class->a->img
+            String date = element.select("post_info_date").first().getElementsByTag("a").first().text();
+
+
+            articles.add(new Article(header, content, link, imageLink, date, Website.BBC));
+        }
+
+        return articles;
+    }
+}
