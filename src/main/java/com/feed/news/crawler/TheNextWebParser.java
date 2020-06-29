007ad74bb951e9a4292bd2f75bf7145f6d103700
zeynab-mammadova <zeynebb2022@gmail.com>
2020-06-29 14:10:24 +0400

    on a process

--- src/main/java/com/feed/news/crawler/TheNextWebParser.java
+++ src/main/java/com/feed/news/crawler/TheNextWebParser.java
@@ -1,36 +1,36 @@
-package com.feed.news.crawler;
-
-import lombok.SneakyThrows;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class TheNextWebParser extends News implements JsoupParser {
-    static List<Article> articles = new ArrayList();
-
-    @SneakyThrows
-    @Override
-    public List<Article> getArticles() {
-        Document document = Jsoup.connect("https://thenextweb.com/latest/").get();
-        Elements elements = document.getElementsByTag("li");
-        for (Element element : elements) {
-            String header = element.select(".story-title").first()
-                    .getElementsByTag("a").first().text();
-            String content = element.select(".story-chunk").text();  //tag--><p>.text()
-            String link =element.select(".story-title").first()
-                    .getElementsByTag("a").first().attr("href");
-//            String imageLink = element.select(".Search-product").first()
-//                    .getElementsByTag("img").first().attr("src");
-            String imageLink = element.select(".lazy story-image").first().attr("src");  //?
-            String date = element.getElementsByTag("time").first().attr("data-full-date");  //datatime
-
-            articles.add(new Article(header, content, link, imageLink, date, Website.TheNextWeb));
-        }
-
-        return articles;
-    }
-}
+//package com.feed.news.crawler;
+//
+//import lombok.SneakyThrows;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class TheNextWebParser extends News implements JsoupParser {
+//    static List<Article> articles = new ArrayList();
+//
+//    @SneakyThrows
+//    @Override
+//    public List<Article> getArticles() {
+//        Document document = Jsoup.connect("https://thenextweb.com/latest/").get();
+//        Elements elements = document.getElementsByTag("li");
+//        for (Element element : elements) {
+////            String header = element.select(".story-title").first()
+////                    .getElementsByTag("a").first().text();
+//            String content = element.select(".story-chunk").text();  //tag--><p>.text()
+//            String link =element.select(".story-title").first()
+//                    .getElementsByTag("a").first().attr("href");
+////            String imageLink = element.select(".Search-product").first()
+////                    .getElementsByTag("img").first().attr("src");
+//            String imageLink = element.select(".lazy story-image").first().attr("src");  //?
+//            String date = element.getElementsByTag("time").first().attr("data-full-date");  //datatime
+//
+//            articles.add(new Article(header, content, link, imageLink, date, Website.TheNextWeb));
+//        }
+//
+//        return articles;
+//    }
+//}
--- src/main/java/com/feed/news/entity/News.java
+++ src/main/java/com/feed/news/entity/News.java
@@ -1,27 +1,27 @@
-package com.feed.news.entity;
-
-import lombok.Data;
-
-import javax.persistence.*;
-import java.util.Set;
-
-@Data
-@Entity
-@Table (name="news")
-public class News {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "news_id")
-    private int news_id;
-
-    @Column (name = "news_url")
-    private String news_url;
-
-    @ManyToMany
-    @JoinTable(name = "disliked",
-    joinColumns = {@JoinColumn(name = "n_id", referencedColumnName ="news_id" )},
-            inverseJoinColumns = {@JoinColumn(name = "u_id", referencedColumnName = "user_id")}
-    )
-    private Set<User> users;
-}
+//package com.feed.news.entity;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//import java.util.Set;
+//
+//@Data
+//@Entity
+//@Table (name="news")
+//public class News {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column (name = "news_id")
+//    private int news_id;
+//
+//    @Column (name = "news_url")
+//    private String news_url;
+//
+//    @ManyToMany
+//    @JoinTable(name = "disliked",
+//    joinColumns = {@JoinColumn(name = "n_id", referencedColumnName ="news_id" )},
+//            inverseJoinColumns = {@JoinColumn(name = "u_id", referencedColumnName = "user_id")}
+//    )
+//    private Set<User> users;
+//}
