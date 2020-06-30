55215b8f9e5c3598c31596d77498b188b2c87a0d
zeynab-mammadova <zeynebb2022@gmail.com>
2020-06-30 04:40:32 +0400

    some data formats problems fixed, except insider,habr sites, i list sites according to view

--- src/main/java/com/feed/news/crawler/DateTimeFormats.java
+++ src/main/java/com/feed/news/crawler/DateTimeFormats.java
@@ -6,7 +6,6 @@ public class DateTimeFormats {
     public static final DateTimeFormatter ABBR_MONTH_FORMAT=DateTimeFormatter.ofPattern("MMM dd, uuuu");
     public static final DateTimeFormatter SIMPLE_MONTH_FORMAT=DateTimeFormatter.ofPattern("d MMMM uuuu");
     public static final DateTimeFormatter SIMPLE_MONTH_FORMAT2=DateTimeFormatter.ofPattern("MMMM d, uuuu");
-    public static final DateTimeFormatter SIMPLE_MONTH_FORMAT3=DateTimeFormatter.ofPattern("MMM d, uuuu");
     public static final DateTimeFormatter FULL_MONTH_FORMAT=DateTimeFormatter.ofPattern("MMMM dd, uuuu");
     public static final DateTimeFormatter HABR_FORMAT=DateTimeFormatter.ofPattern("MMMM dd, uuuu 'at' hh:mm a");
     public static final DateTimeFormatter ISO_INSTANT_FORMAT=DateTimeFormatter.ofPattern("MMM d, uuuu, hh:mm a");
