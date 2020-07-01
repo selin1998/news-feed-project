1e1c1f74229cce0c66cebe1ce469db792d2abdad
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-07-01 23:51:26 +0400

    -- disable-news page works and data added to DB.Just one problem with html file.I will fix it=)

--- src/main/java/com/feed/news/bean/AutoRun.java
+++ src/main/java/com/feed/news/bean/AutoRun.java
@@ -50,50 +50,50 @@ public class AutoRun {
 
 
 
-    @Bean
-    @Order(1)
+//    @Bean
+//    @Order(1)
     public CommandLineRunner add_news(){
         return args->{
             newsRepo.saveAll(Arrays.asList(
                     new News("TechCrunch","https://techcrunch.com/"
                     ,"TechCrunch - Reporting on the business of technology, startups, venture capital funding, and Silicon Valley"
-                    ,convertImagetoByteArray("techcrunch_logo.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/techcrunch_logo.png")),
 
                     new News("UberGizmo","https://www.ubergizmo.com/"
                     ,"Opinionated news and reviews of consumer electronics"
-                    ,convertImagetoByteArray("ubergizmo_logo_icon_64.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/ubergizmo_logo_icon_64.png")),
 
                     new News("DroidLife","https://www.droid-life.com/"
                     ,"We write about the happenings in the Android world, most of which involves Google. " +
                             "We do reviews too, so we got you covered, Samsung guy."
-                    , convertImagetoByteArray("droidlife_logo.png")),
+                    , convertImagetoByteArray("src/main/resources/templates/img/droidlife_logo.png")),
 
                     new News("TechStartups","https://techstartups.com/"
                     ,"TechStartups - Coverage of Technology News, technology startups, Emerging technology, venture capital funding, and Silicon Valley"
-                    ,convertImagetoByteArray("techstartups.com-logo-v3.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/techstartups.com-logo-v3.png")),
 
                     new News("LenovaNews","https://news.lenovo.com/pressroom/press-releases/"
                     ,"Lenovo's official site for press materials and original stories about the vision and passion behind the technology."
-                    ,convertImagetoByteArray("lenovo2_logo.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/lenovo2_logo.png")),
 
                     new News("Insider","https://www.insider.com/news","What You Need To Know"
-                            ,convertImagetoByteArray("insider_logo.png")),
+                            ,convertImagetoByteArray("src/main/resources/templates/img/insider_logo.png")),
 
                     new News("HTCNews","https://www.pocket-lint.com/htc"
                     ,"Get all the latest on HTC. All the very best in HTC news, HTC reviews, HTC rumours and HTC buying advice in one place."
-                    ,convertImagetoByteArray("HTC_logo.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/HTC_logo.png")),
 
                     new News("DigitIn","https://www.digit.in/news/"
                             ,"Latest Technology News on mobile phones in India. Read news on computer, apps, games, gadgets and other personal tech."
-                    ,convertImagetoByteArray("digit_logo.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/digit_logo.png")),
 
                     new News("Policy","https://www.policygenius.com/blog/"
                     ,"A financial newsletter bringing you the money news and the money moves."
-                    ,convertImagetoByteArray("policy_logo.png")),
+                    ,convertImagetoByteArray("src/main/resources/templates/img/policy_logo.png")),
 
             new News("Habr","https://habr.com/en/flows/develop/"
                     ,"All posts in Development stream on Habr"
-                    ,convertImagetoByteArray("Habr_logo.png"))
+                    ,convertImagetoByteArray("src/main/resources/templates/img/Habr_logo.png"))
             ));
 
         };
@@ -106,10 +106,11 @@ public class AutoRun {
 
             return args->{
                 Set<News> newsSet=new HashSet<>(Arrays.asList(new News("Habr","https://habr.com/en/"
-                        ,"All posts in Development stream on Habr",convertImagetoByteArray("Habr_logo.png"))
+                        ,"All posts in Development stream on Habr"
+                                ,convertImagetoByteArray("src/main/resources/templates/img/Habr_logo.png"))
                         ,new News("Policy","https://www.policygenius.com/blog/"
                                 ,"A financial newsletter bringing you the money news and the money moves."
-                                ,convertImagetoByteArray("policy_logo.png"))));
+                                ,convertImagetoByteArray("src/main/resources/templates/img/policy_logo.png"))));
                 User user1=new User("sherlock holmes","sholmes@gmail.com","watson",newsSet);
                 userRepo.save(user1);
 
